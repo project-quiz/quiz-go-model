9f13e742cf0b12619e57dc4e35697a455069d465
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2019-01-12 22:22:58 +0100

    Added  PlayerJoin message for users that want to join the server.

--- Player/Player.pb.go
+++ Player/Player.pb.go
@@ -67,6 +67,46 @@ func (m *Player) GetNickname() string {
 	return ""
 }
 
+type PlayerJoin struct {
+	//Add all data that the user needs to fill in before joining. (avatar? etc.)
+	Nickname             string   `protobuf:"bytes,1,opt,name=nickname,proto3" json:"nickname,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *PlayerJoin) Reset()         { *m = PlayerJoin{} }
+func (m *PlayerJoin) String() string { return proto.CompactTextString(m) }
+func (*PlayerJoin) ProtoMessage()    {}
+func (*PlayerJoin) Descriptor() ([]byte, []int) {
+	return fileDescriptor_b2d9cd5c5d233ec9, []int{1}
+}
+
+func (m *PlayerJoin) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_PlayerJoin.Unmarshal(m, b)
+}
+func (m *PlayerJoin) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_PlayerJoin.Marshal(b, m, deterministic)
+}
+func (m *PlayerJoin) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_PlayerJoin.Merge(m, src)
+}
+func (m *PlayerJoin) XXX_Size() int {
+	return xxx_messageInfo_PlayerJoin.Size(m)
+}
+func (m *PlayerJoin) XXX_DiscardUnknown() {
+	xxx_messageInfo_PlayerJoin.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_PlayerJoin proto.InternalMessageInfo
+
+func (m *PlayerJoin) GetNickname() string {
+	if m != nil {
+		return m.Nickname
+	}
+	return ""
+}
+
 type PlayerJoined struct {
 	Guid                 string   `protobuf:"bytes,1,opt,name=guid,proto3" json:"guid,omitempty"`
 	Player               *Player  `protobuf:"bytes,2,opt,name=player,proto3" json:"player,omitempty"`
@@ -79,7 +119,7 @@ func (m *PlayerJoined) Reset()         { *m = PlayerJoined{} }
 func (m *PlayerJoined) String() string { return proto.CompactTextString(m) }
 func (*PlayerJoined) ProtoMessage()    {}
 func (*PlayerJoined) Descriptor() ([]byte, []int) {
-	return fileDescriptor_b2d9cd5c5d233ec9, []int{1}
+	return fileDescriptor_b2d9cd5c5d233ec9, []int{2}
 }
 
 func (m *PlayerJoined) XXX_Unmarshal(b []byte) error {
@@ -126,7 +166,7 @@ func (m *PlayerLeft) Reset()         { *m = PlayerLeft{} }
 func (m *PlayerLeft) String() string { return proto.CompactTextString(m) }
 func (*PlayerLeft) ProtoMessage()    {}
 func (*PlayerLeft) Descriptor() ([]byte, []int) {
-	return fileDescriptor_b2d9cd5c5d233ec9, []int{2}
+	return fileDescriptor_b2d9cd5c5d233ec9, []int{3}
 }
 
 func (m *PlayerLeft) XXX_Unmarshal(b []byte) error {
@@ -173,7 +213,7 @@ func (m *PlayersNickNameChanged) Reset()         { *m = PlayersNickNameChanged{}
 func (m *PlayersNickNameChanged) String() string { return proto.CompactTextString(m) }
 func (*PlayersNickNameChanged) ProtoMessage()    {}
 func (*PlayersNickNameChanged) Descriptor() ([]byte, []int) {
-	return fileDescriptor_b2d9cd5c5d233ec9, []int{3}
+	return fileDescriptor_b2d9cd5c5d233ec9, []int{4}
 }
 
 func (m *PlayersNickNameChanged) XXX_Unmarshal(b []byte) error {
@@ -210,6 +250,7 @@ func (m *PlayersNickNameChanged) GetPlayer() *Player {
 
 func init() {
 	proto.RegisterType((*Player)(nil), "Data.Player")
+	proto.RegisterType((*PlayerJoin)(nil), "Data.PlayerJoin")
 	proto.RegisterType((*PlayerJoined)(nil), "Data.PlayerJoined")
 	proto.RegisterType((*PlayerLeft)(nil), "Data.PlayerLeft")
 	proto.RegisterType((*PlayersNickNameChanged)(nil), "Data.PlayersNickNameChanged")
@@ -218,15 +259,16 @@ func init() {
 func init() { proto.RegisterFile("Player.proto", fileDescriptor_b2d9cd5c5d233ec9) }
 
 var fileDescriptor_b2d9cd5c5d233ec9 = []byte{
-	// 156 bytes of a gzipped FileDescriptorProto
+	// 167 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x09, 0xc8, 0x49, 0xac,
 	0x4c, 0x2d, 0xd2, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x71, 0x49, 0x2c, 0x49, 0x54, 0xb2,
 	0xe0, 0x62, 0x83, 0x88, 0x0a, 0x09, 0x71, 0xb1, 0xa4, 0x97, 0x66, 0xa6, 0x48, 0x30, 0x2a, 0x30,
 	0x6a, 0x70, 0x06, 0x81, 0xd9, 0x42, 0x52, 0x5c, 0x1c, 0x79, 0x99, 0xc9, 0xd9, 0x79, 0x89, 0xb9,
-	0xa9, 0x12, 0x4c, 0x60, 0x71, 0x38, 0x5f, 0xc9, 0x03, 0x66, 0x9e, 0x57, 0x7e, 0x66, 0x5e, 0x6a,
-	0x0a, 0x56, 0xfd, 0x2a, 0x5c, 0x6c, 0x05, 0x60, 0x35, 0x60, 0xdd, 0xdc, 0x46, 0x3c, 0x7a, 0x20,
-	0x4b, 0xf5, 0x20, 0xfa, 0x82, 0xa0, 0x72, 0x4a, 0x6e, 0x5c, 0x5c, 0x10, 0x11, 0x9f, 0xd4, 0xb4,
-	0x12, 0x0a, 0xcc, 0x09, 0xe2, 0x12, 0x83, 0x88, 0x14, 0xfb, 0x65, 0x26, 0x67, 0xfb, 0x25, 0xe6,
-	0xa6, 0x3a, 0x67, 0x24, 0xe6, 0xa5, 0x53, 0xe2, 0xb6, 0x24, 0x36, 0x70, 0x60, 0x19, 0x03, 0x02,
-	0x00, 0x00, 0xff, 0xff, 0x0f, 0xba, 0xa6, 0x14, 0x3c, 0x01, 0x00, 0x00,
+	0xa9, 0x12, 0x4c, 0x60, 0x71, 0x38, 0x5f, 0x49, 0x83, 0x8b, 0x0b, 0xa2, 0xd3, 0x2b, 0x3f, 0x33,
+	0x0f, 0x45, 0x25, 0x23, 0x9a, 0x4a, 0x0f, 0x98, 0xcd, 0x20, 0x95, 0xa9, 0x29, 0x58, 0x6d, 0x52,
+	0xe1, 0x62, 0x2b, 0x00, 0xab, 0x01, 0xdb, 0xc3, 0x6d, 0xc4, 0xa3, 0x07, 0x72, 0x9e, 0x1e, 0x44,
+	0x5f, 0x10, 0x54, 0x4e, 0xc9, 0x0d, 0x66, 0xa7, 0x4f, 0x6a, 0x5a, 0x09, 0x05, 0xe6, 0x04, 0x71,
+	0x89, 0x41, 0x44, 0x8a, 0xfd, 0x32, 0x93, 0xb3, 0xfd, 0x12, 0x73, 0x53, 0x9d, 0x33, 0x12, 0xf3,
+	0xd2, 0x29, 0x71, 0x5b, 0x12, 0x1b, 0x38, 0x58, 0x8d, 0x01, 0x01, 0x00, 0x00, 0xff, 0xff, 0xc8,
+	0x10, 0x50, 0x8f, 0x66, 0x01, 0x00, 0x00,
 }
